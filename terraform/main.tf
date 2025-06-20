10c625806626a26591b017bbc6258b880ccb9622
ifaakash <aakashc.appregis@gmail.com>
2025-06-21 00:57:51 +0530

    chore(git): Add .gitignore file with required terraform file listed

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,7 @@ module "lambda" {
   lambda_exec_role_arn = module.iam.lambda_exec_role_arn
 }
 
+# API Gateway
 module "apigw" {
   source            = "./modules/apigateway"
   lambda_name       = module.lambda.name
