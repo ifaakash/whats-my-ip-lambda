a8f81bfe0d3fb1f63bab9335948f328b98d200e5
ifaakash <aakashc.appregis@gmail.com>
2025-06-21 00:52:24 +0530

    chore(terraform): Add comments in modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,9 @@
+# Module for IAM Role and Policy for Lambda
 module "iam" {
   source = "./modules/iam"
 }
 
+# Lambda Configuration
 module "lambda" {
   source               = "./modules/lambda"
   lambda_exec_role_arn = module.iam.lambda_exec_role_arn
